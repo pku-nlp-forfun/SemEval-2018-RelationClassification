24240747bd41aa89e120bec31b3b4c97484b084b
gunjianpan <iofu728@163.com>
2019-04-08 02:16:54 +0800

    🕵add embedding dir

--- constant.py
+++ constant.py
@@ -8,7 +8,8 @@ test_data_path = 'data/test/'
 train_data_path = 'data/'
 prediction_path = 'prediction/'
 score_script_path = 'data/semeval2018_task7_scorer-v1.2.pl'
-pickle_path = 'pickle/' # embedding.
+pickle_path = 'pickle/'  # embedding.
+embedding_dir = '../LightRel//SemEval18task7/feature/'
 
 embedding_dim = 300
 
@@ -26,18 +27,19 @@ id2rela = {
     1: 'TOPIC',
     2: 'RESULT',
     3: 'PART_WHOLE',
-    4: 'MODEL-FEATURE', # MODEL
+    4: 'MODEL-FEATURE',  # MODEL
     5: 'COMPARE'        # COMPARISON
 }
 
 entity_relation = {
-    0: ['used by', 'used for', 'applied to', 'performed on'], # USAGE
-    1: ['presents', 'of'], # TOPIC
-    2: ['affects', 'problem', 'yields'], # RESULT
-    3: ['composed of', 'extracted from', 'found in'], # PART_WHOLE
-    4: ['of an observed', 'of', 'associated to'], # MODEL-FEATURE (MODEL)
-    5: ['compared to'], # COMPARE (COMPARISON)
+    0: ['used by', 'used for', 'applied to', 'performed on'],  # USAGE
+    1: ['presents', 'of'],  # TOPIC
+    2: ['affects', 'problem', 'yields'],  # RESULT
+    3: ['composed of', 'extracted from', 'found in'],  # PART_WHOLE
+    4: ['of an observed', 'of', 'associated to'],  # MODEL-FEATURE (MODEL)
+    5: ['compared to'],  # COMPARE (COMPARISON)
 }
+
 
 def key_path(key=1):
     return '%skeys.test.1.%d.txt' % (test_data_path, key)
